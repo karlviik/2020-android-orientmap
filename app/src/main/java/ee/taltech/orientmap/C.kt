42559037bacb2f8a81df7e6718f68f86d8794c2d
karlviik <viik.karl2@gmail.com>
2020-03-27 22:47:17 +0200

    Mash demos together

---
+++ app/src/main/java/ee/taltech/orientmap/C.kt
@@ -0,0 +1,16 @@
+package ee.taltech.orientmap
+
+class C {
+    companion object {
+        const val NOTIFICATION_CHANNEL = "default_channel"
+
+        const val NOTIFICATION_ACTION_WP = "ee.taltech.kaviik.orientmap.wp"
+        const val NOTIFICATION_ACTION_CP = "ee.taltech.kaviik.orientmap.cp"
+        const val LOCATION_UPDATE_ACTION = "ee.taltech.kaviik.orientmap.location_update"
+        const val LOCATION_UPDATE_ACTION_LATITUDE = "ee.taltech.kaviik.orientmap.location_update.latitude"
+        const val LOCATION_UPDATE_ACTION_LONGITUDE = "ee.taltech.kaviik.orientmap.location_update.longitude"
+
+        const val NOTIFICATION_ID = 4321
+        const val REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+    }
+}
