755433c7abbbd76f3a6e5f80284b4e5365374ea3
karlviik <viik.karl2@gmail.com>
2020-05-10 16:17:32 +0300

    Implement logging in

--- app/src/main/java/ee/taltech/orientmap/activities/LoginActivity.kt
+++ app/src/main/java/ee/taltech/orientmap/activities/LoginActivity.kt
@@ -1,9 +1,16 @@
 package ee.taltech.orientmap.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.android.volley.Response
 import ee.taltech.orientmap.R
+import ee.taltech.orientmap.utils.ApiUtils
+import ee.taltech.orientmap.utils.PreferenceUtils
+import kotlinx.android.synthetic.main.activity_login.view.*
+import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
 	
@@ -12,6 +19,28 @@ class LoginActivity : AppCompatActivity() {
 		setContentView(R.layout.activity_login)
 	}
 	
-	fun onClickMoveToRegister(view: View) {}
-	fun onClickPerformLogin(view: View) {}
+	fun onClickMoveToRegister(view: View) {
+		val intent = Intent(this, RegisterActivity::class.java)
+		startActivity(intent)
+	}
+	
+	fun onClickPerformLogin(view: View) {
+		val password = view.editTextPassword.text.toString()
+		val email = view.editTextEmail.text.toString()
+		val listener = Response.Listener<JSONObject> { response ->
+			Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show()
+			val token = response.getString("token")
+			PreferenceUtils.setUserEmail(this, email)
+			PreferenceUtils.setLoggedIn(this, true)
+			PreferenceUtils.setToken(this, token)
+			this.finish()
+		}
+		
+		val errorListener = Response.ErrorListener { error ->
+			// TODO: maybe break down different errors?
+			Toast.makeText(this, "Error trying to log in!\n" + error, Toast.LENGTH_SHORT).show()
+		}
+		ApiUtils.loginUser(this, listener, errorListener, email, password)
+		
+	}
 }
